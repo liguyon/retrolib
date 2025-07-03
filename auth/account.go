80cc9412ba93feb17813105d531c73c224809200
liguyon <hello@liguyon.dev>
2025-07-03 19:53:32 -0400

    fix a typo in a comment

--- auth/account.go
+++ auth/account.go
@@ -12,7 +12,7 @@ const (
 	ErrDisconnectedAccount       LoginErrorID = 'd' // Disconnected a character using this account
 	ErrKicked                    LoginErrorID = 'k' // Kicked vs Banned? Ban temp vs ban def?
 	ErrServerFull                LoginErrorID = 'w' // Login server full
-	ErrOldAccount                LoginErrorID = 'o' // Account created before AG enforced login though AG account?
+	ErrOldAccount                LoginErrorID = 'o' // Account created before AG enforced login through AG account?
 	ErrOldAccountUseNew          LoginErrorID = 'e' // Game account linked to AG account, must use AG account?
 	ErrMaintainAccount           LoginErrorID = 'm' // Account under maintenance
 	ErrChooseNickname            LoginErrorID = 'r' // No nickname chosen yet
